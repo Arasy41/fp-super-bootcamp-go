3d16a86c90794e67d6c40c814f2ec8fc37fe3b50
Artzy1401 <afifarsyi@gmail.com>
2024-07-08 17:34:10 +0700

    Change host for deployn web app

--- docs/docs.go
+++ docs/docs.go
@@ -1468,7 +1468,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "unsightly-delores-arasycorp-a5cc0ffe.koyeb.app/",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "API Culinary Review",
